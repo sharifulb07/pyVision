f0296d8e7a47840cd7e499885c84735db4a2f3a2
shariful islam <sharifulb07@gmail.com>
2022-10-02 09:14:44 +0600

    Initial commit

---
+++ userInput.py
@@ -0,0 +1,6 @@
+name=input("Enter your user name: ")
+age=input("enter your age:")
+GPA=input("Enter your GPA here okay")
+profession=input("enter your profession")
+print(name+"\n"+age+'\n'+GPA+"\n"+profession)
+
